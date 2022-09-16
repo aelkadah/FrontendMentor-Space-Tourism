cf97e3f9953227afd012291922f447f58454591f
AhmedElKadah <ahemdaymanelakdah@gmail.com>
2022-09-16 15:54:58 +0200

    navigation menu set active based on url

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-// document.onload = setActive();
-// function setActive() {
-//   console.log(`aa`);
-//   aObj = document.querySelectorAll("ul li a");
-//   for (i = 0; i < aObj.length; i++) {
-//     if (document.location.href.indexOf(aObj[i].href) >= 0) {
-//       aObj.forEach((ele) => {
-//         ele.classList.remove("active");
-//       });
-//       aObj[i].classList.add("active");
-//     }
-//   }
-// }
+document.onload = setActive();
+function setActive() {
+  let currentLocation = location.href;
+  let menuLis = document.querySelectorAll("li");
+  let menuLisA = document.querySelectorAll("li a");
+  for (let i = 0; i < menuLis.length; i++) {
+    if (menuLisA[i].href === currentLocation) {
+      menuLis[i].classList.add("active");
+    }
+  }
+}
 
 // Vertical Navigation
 let menuBtn = document.querySelector(".menuBtn a");
